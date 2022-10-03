077006c605c19875f01caf216255e367a3f54cd5
GenYans <genapiter@yandex.ru>
2022-10-03 11:49:19 +0300

    Task №3.2

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,6 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+});
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
