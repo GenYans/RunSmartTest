60bba6238f6500178b850a6449ab6890c6e3e2b6
GenYans <genapiter@yandex.ru>
2022-10-03 16:04:30 +0300

    Task №3.3

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        /* adaptiveHeight: true, */
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left.png"</button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right.png"</button>',
         responsive: [
@@ -17,7 +17,20 @@ $(document).ready(function(){
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
+          .eq($(this).index()).addClass('catalog__content_active');
       });
-  });
-      
+
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
