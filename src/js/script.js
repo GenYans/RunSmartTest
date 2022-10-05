33c01de690f7038761f8856abb832bde7cbfb139
GenYans <genapiter@yandex.ru>
2022-10-05 14:45:12 +0300

    Task №5

--- src/js/script.js
+++ src/js/script.js
@@ -2,26 +2,27 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left.png"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right.png"</button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right.png"></button>',
         responsive: [
             {
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
                 }
             }
         ]
-      });
-      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
-          .eq($(this).index()).addClass('catalog__content_active');
-      });
+          .closest('div.container').find('div.catalog__content')
+          .removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-      function toggleSlide(item) {
+    function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
                 e.preventDefault();
@@ -33,4 +34,41 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
-});
+
+    //Modal 
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    $('#consultation-form').validate();
+    $('#consultation form').validate({
+        rules: {
+            name: "required",
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "Пожалуйста, введите свое имя",
+            phone: "Пожалуйста введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свой почтовый адрес",
+              email: "Неправильно введен адрес почты"
+            }
+          }
+    });
+    $('#order form').validate();
+}); 
