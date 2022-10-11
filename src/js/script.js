b30b6a81b380e31daac6f11c81c67570c1070153
GenYans <genapiter@yandex.ru>
2022-10-11 15:09:38 +0300

    Task №6.5

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,11 @@ $(document).ready(function(){
 
     $('form').submit(function(e) {
         e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'gennadyanelis@gmail.com';                 // Наш логин
+$mail->Password = 'wnndbcvdnhizkyoh';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('gennadyanelis@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('yanelisgen@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
