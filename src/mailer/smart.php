822d54b966960cbf3d078d41eb3e0811b88f2281
GenYans <genapiter@yandex.ru>
2022-10-13 13:37:38 +0300

    Task №6.5

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'gennadyanelis@gmail.com';                 // Наш логин
-$mail->Password = 'wnndbcvdnhizkyoh';                           // Наш пароль от ящика
+$mail->Password = 'Gennadiy201020152010';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('gennadyanelis@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('yanelisgen@gmail.com');     // Add a recipient
+$mail->addAddress('sodole1121@cebaike.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
