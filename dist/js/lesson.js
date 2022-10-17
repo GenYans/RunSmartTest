3f1e77a177fccb20de0ba38a9a4a0153167e3cea
GenYans <genapiter@yandex.ru>
2022-10-17 16:35:53 +0300

    Task №9

---
+++ dist/js/lesson.js
@@ -0,0 +1,53 @@
+/* Типы данных  */
+
+/* number - числа 
+
+/* null - элемент которого не существует */
+
+/* undefined - существует, но не имеет никакого значения  */
+
+/* {} - объект, комплексный
+Пример - описание структуры 
+let obj = {
+    name: 'apple',
+    color: 'green',
+    weight: 200,
+} */
+
+/* Symbol - символ, технический  - редко используется  */
+
+/* Пример 1 */
+
+/* let answer = confirm("Вам есть 18 лет?")
+if (answer) {
+    console.log('Доступ открыт')
+}else {
+    console.log('Нет доступа')
+}
+
+/* Пример 2 */
+/* const num = 50;
+
+if (num < 49) {
+    console.log('Не правильно')
+} else if (num > 100) {
+    console.log('Много')
+}else {
+    console.log('Верно')
+} */
+
+/* Циклы */
+
+/* for (let i = 1; i < 8; i++) {
+    console.log(i);
+}  */
+
+/* Функции  */
+
+/* function logging(a,b) {
+    console.log( a + b )
+}
+logging(3, 5);
+
+logging(4, 12); */
+
